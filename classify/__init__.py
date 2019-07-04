46af562d4c2ae3f030d4a0f6b6da50d6f6cfd9b1
Anthony Chu <antchu@microsoft.com>
2019-07-04 14:45:19 -0700

    Update function
    Co-authored-by: Asavari Tayal <tayal1asavari@gmail.com>

--- classify/__init__.py
+++ classify/__init__.py
@@ -2,23 +2,21 @@ import logging
 
 import azure.functions as func
 
+import json
+import os
+import io
+
+# Import helper script
+from . import predict
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # Initialize model
+    predict.initialize()
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    image_url = req.params.get('img')
+    # Scoring step
+    results = predict.predict_url(image_url)
+
+    return json.dumps(results)
