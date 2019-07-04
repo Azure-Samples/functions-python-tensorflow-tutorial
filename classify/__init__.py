7044bd2ebdf811d1a7635d15090b51983cd0e7f6
Anthony Chu <antchu@microsoft.com>
2019-07-04 15:09:18 -0700

    Minor refactoring

--- classify/__init__.py
+++ classify/__init__.py
@@ -3,8 +3,6 @@ import logging
 import azure.functions as func
 
 import json
-import os
-import io
 
 # Import helper script
 from . import predict
@@ -12,11 +10,8 @@ from . import predict
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # Initialize model
-    predict.initialize()
-
     image_url = req.params.get('img')
     # Scoring step
-    results = predict.predict_url(image_url)
+    results = predict.predict_image_from_url(image_url)
 
     return json.dumps(results)
--- classify/predict.py
+++ classify/predict.py
@@ -28,7 +28,7 @@ input_node = 'Placeholder:0'
 graph_def = tf.GraphDef()
 labels = []
 
-def initialize():
+def _initialize():
     global labels
     if not labels:
         with tf.gfile.FastGFile(filename, 'rb') as f:
@@ -37,10 +37,10 @@ def initialize():
         with open(labels_filename, 'rt') as lf:
             labels = [l.strip() for l in lf.readlines()]
 
-def log_msg(msg):
+def _log_msg(msg):
     logging.info("{}: {}".format(datetime.now(),msg))
 
-def extract_bilinear_pixel(img, x, y, ratio, xOrigin, yOrigin):
+def _extract_bilinear_pixel(img, x, y, ratio, xOrigin, yOrigin):
     xDelta = (x + 0.5) * ratio - 0.5
     x0 = int(xDelta)
     xDelta -= x0
@@ -82,7 +82,7 @@ def extract_bilinear_pixel(img, x, y, ratio, xOrigin, yOrigin):
     pixel = yDelta * t + (1. - yDelta) * b
     return pixel.astype(np.uint8)
 
-def extract_and_resize(img, targetSize):
+def _extract_and_resize(img, targetSize):
     determinant = img.shape[1] * targetSize[0] - img.shape[0] * targetSize[1]
     if determinant < 0:
         ratio = float(img.shape[1]) / float(targetSize[1])
@@ -99,54 +99,47 @@ def extract_and_resize(img, targetSize):
     resize_image = np.empty((targetSize[0], targetSize[1], img.shape[2]), dtype=np.uint8)
     for y in range(targetSize[0]):
         for x in range(targetSize[1]):
-            resize_image[y, x] = extract_bilinear_pixel(img, x, y, ratio, xOrigin, yOrigin)
+            resize_image[y, x] = _extract_bilinear_pixel(img, x, y, ratio, xOrigin, yOrigin)
     return resize_image
 
-def extract_and_resize_to_256_square(image):
+def _extract_and_resize_to_256_square(image):
     h, w = image.shape[:2]
-    log_msg("crop_center: " + str(w) + "x" + str(h) +" and resize to " + str(256) + "x" + str(256))
-    return extract_and_resize(image, (256, 256))
+    _log_msg("crop_center: " + str(w) + "x" + str(h) +" and resize to " + str(256) + "x" + str(256))
+    return _extract_and_resize(image, (256, 256))
 
-def crop_center(img,cropx,cropy):
+def _crop_center(img,cropx,cropy):
     h, w = img.shape[:2]
     startx = max(0, w//2-(cropx//2) - 1)
     starty = max(0, h//2-(cropy//2) - 1)
-    log_msg("crop_center: " + str(w) + "x" + str(h) +" to " + str(cropx) + "x" + str(cropy))
+    _log_msg("crop_center: " + str(w) + "x" + str(h) +" to " + str(cropx) + "x" + str(cropy))
     return img[starty:starty+cropy, startx:startx+cropx]
 
-def resize_down_to_1600_max_dim(image):
+def _resize_down_to_1600_max_dim(image):
     w,h = image.size
     if h < 1600 and w < 1600:
         return image
 
     new_size = (1600 * w // h, 1600) if (h > w) else (1600, 1600 * h // w)
-    log_msg("resize: " + str(w) + "x" + str(h) + " to " + str(new_size[0]) + "x" + str(new_size[1]))
+    _log_msg("resize: " + str(w) + "x" + str(h) + " to " + str(new_size[0]) + "x" + str(new_size[1]))
     if max(new_size) / max(image.size) >= 0.5:
         method = Image.BILINEAR
     else:
         method = Image.BICUBIC
     return image.resize(new_size, method)
 
-def predict_url(imageUrl):
-    logging.info("Predicting from url: " +imageUrl)
-
-    with urlopen(imageUrl) as testImage:
-        image = Image.open(testImage)
-        return predict_image(image)
-
-def convert_to_nparray(image):
+def _convert_to_nparray(image):
     # RGB -> BGR
-    log_msg("Convert to numpy array")
+    _log_msg("Convert to numpy array")
     image = np.array(image)
     return image[:, :, (2,1,0)]
 
-def update_orientation(image):
+def _update_orientation(image):
     exif_orientation_tag = 0x0112
     if hasattr(image, '_getexif'):
         exif = image._getexif()
         if exif != None and exif_orientation_tag in exif:
             orientation = exif.get(exif_orientation_tag, 1)
-            log_msg('Image has EXIF Orientation: ' + str(orientation))
+            _log_msg('Image has EXIF Orientation: ' + str(orientation))
             # orientation is 1 based, shift to zero based and flip/transpose based on 0-based values
             orientation -= 1
             if orientation >= 4:
@@ -157,32 +150,32 @@ def update_orientation(image):
                 image = image.transpose(Image.FLIP_LEFT_RIGHT)
     return image
                 
-def predict_image(image):
+def _predict_image(image):
         
-    log_msg('Predicting image')
+    _log_msg('Predicting image')
     try:
         if image.mode != "RGB":
-            log_msg("Converting to RGB")
+            _log_msg("Converting to RGB")
             image.convert("RGB")
 
         w,h = image.size
-        log_msg("Image size: " + str(w) + "x" + str(h))
+        _log_msg("Image size: " + str(w) + "x" + str(h))
         
         # Update orientation based on EXIF tags
-        image = update_orientation(image)
+        image = _update_orientation(image)
 
         # If the image has either w or h greater than 1600 we resize it down respecting
         # aspect ratio such that the largest dimention is 1600
-        image = resize_down_to_1600_max_dim(image)
+        image = _resize_down_to_1600_max_dim(image)
 
         # Convert image to numpy array
-        image = convert_to_nparray(image)
+        image = _convert_to_nparray(image)
         
         # Crop the center square and resize that square down to 256x256
-        resized_image = extract_and_resize_to_256_square(image)
+        resized_image = _extract_and_resize_to_256_square(image)
 
         # Crop the center for the specified network_input_Size
-        cropped_image = crop_center(resized_image, network_input_size, network_input_size)
+        cropped_image = _crop_center(resized_image, network_input_size, network_input_size)
 
         tf.reset_default_graph()
         tf.import_graph_def(graph_def, name='')
@@ -209,9 +202,19 @@ def predict_image(image):
                 'predictions': result 
             }
 
-            log_msg("Results: " + str(response))
+            _log_msg("Results: " + str(response))
             return response
             
     except Exception as e:
-        log_msg(str(e))
+        _log_msg(str(e))
         return 'Error: Could not preprocess image for prediction. ' + str(e)
+
+def predict_image_from_url(image_url):
+    logging.info("Predicting from url: " + image_url)
+
+    _initialize()
+
+    with urlopen(image_url) as testImage:
+        image = Image.open(testImage)
+        return _predict_image(image)
+
